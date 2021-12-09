2577106527f20fac1bf68ad4ac61116189cc18e5
ChildOfTheGrim <83035641+ChildOfTheGrim@users.noreply.github.com>
2021-12-09 19:26:02 +0100

    select popravljen

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -11,16 +11,47 @@ dugme.addEventListener("click", function () {
     dugme.innerHTML = " [Saznaj vise]";
   }
 });
-//gore
-$(document).ready(function () {
-  jQuery(window).scroll(function () {
-    if (jQuery(window).scrollTop() > 150) {
-      jQuery("#backtotop").addClass("visible");
-    } else {
-      jQuery("#backtotop").removeClass("visible");
-    }
-  });
-});
+//forma
+let padajuci = document.getElementById("padajuca_lista");
+
+var opcije = [
+    "...",
+    "Redovan Servis",
+    "Programiranje Biosa",
+    "Servis Matičnih Ploča",
+    "Spašavanje Podataka",
+    "Instalacija OS",
+    "Popravka uređaja"];
+  var vrednosti = [
+    "prazno",
+    "servis",
+    "bios",
+    "maticna",
+    "podaci",
+    "instalacija",
+    "popravka"];
+
+    let lista = document.createElement("select");
+    lista.setAttribute("name", "problem");
+    lista.setAttribute("id", "problem");
+    lista.setAttribute("required", "required");
+
+for (let i = 0; i < opcije.length; i++) {
+    var novaOpcija = document.createElement("option");
+    novaOpcija.setAttribute("value",  vrednosti[i]);
+    novaOpcija.innerHTML = opcije[i];
+    lista.appendChild(novaOpcija);
+}
+
+for (let i = 0; i < opcije.lenght; i++) {
+  var novaOpcija = document.createElement("option");
+  novaOpcija.setAttribute("value", `${vrednosti[i]}`);
+  var novaOpcijaOpis = document.createTextNode(`${opcije[i]}`);
+  novaOpcija.appendChild(novaOpcijaOpis);
+  lista.appendChild(novaOpcija);
+}
+let padajucaLista = document.getElementById("padajuca_lista");
+padajucaLista.appendChild(lista);
 //menjanje texta
 var recenica = document.getElementById("recenica");
 var text = [
@@ -38,33 +69,40 @@ function Smenjivanje() {
 }
 setInterval(Smenjivanje, 2000);
 //jqveri modal
-$(function(){
+$(function () {
+  var appendthis = "<div class='modal-overlay js-modal-close'></div>";
 
-  var appendthis =  ("<div class='modal-overlay js-modal-close'></div>");
-  
-    $('a[data-modal-id]').click(function(e) {
-      e.preventDefault();
-      $("body").append(appendthis);
-      $(".modal-overlay").fadeTo(500, 0.7);
-      //$(".js-modalbox").fadeIn(500);
-      var modalBox = $(this).attr('data-modal-id');
-      $('#'+modalBox).fadeIn($(this).data());
-    });  
-    
-    
-  $(".js-modal-close, .modal-overlay").click(function() {
-    $(".modal-box, .modal-overlay").fadeOut(500, function() {
+  $("a[data-modal-id]").click(function (e) {
+    e.preventDefault();
+    $("body").append(appendthis);
+    $(".modal-overlay").fadeTo(500, 0.7);
+    //$(".js-modalbox").fadeIn(500);
+    var modalBox = $(this).attr("data-modal-id");
+    $("#" + modalBox).fadeIn($(this).data());
+  });
+
+  $(".js-modal-close, .modal-overlay").click(function () {
+    $(".modal-box, .modal-overlay").fadeOut(500, function () {
       $(".modal-overlay").remove();
     });
   });
-   
-  $(window).resize(function() {
+
+  $(window).resize(function () {
     $(".modal-box").css({
       top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
-      left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+      left: ($(window).width() - $(".modal-box").outerWidth()) / 2,
     });
   });
-   
+
   $(window).resize();
-   
+});
+//jqvery back to top
+$(document).ready(function () {
+  jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 150) {
+      jQuery("#backtotop").addClass("visible");
+    } else {
+      jQuery("#backtotop").removeClass("visible");
+    }
   });
+});
