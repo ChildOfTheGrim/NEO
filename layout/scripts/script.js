346eaeeb7d0b41c1ca3ace9622cc9dad577109cf
ChildOfTheGrim <83035641+ChildOfTheGrim@users.noreply.github.com>
2021-12-09 16:04:12 +0100

    Add files via upload

---
+++ layout/scripts/script.js
@@ -0,0 +1,34 @@
+//saznaj vise
+let dugme = document.getElementById("more_dugme");
+let dodatniText = document.getElementById("text_vise");
+dodatniText.style.display = "none";
+dugme.addEventListener("click", function () {
+  if (dodatniText.style.display === "none") {
+    dodatniText.style.display = "block";
+    dugme.innerHTML = " [Smanji]";
+  } else {
+    dodatniText.style.display = "none";
+    dugme.innerHTML = " [Saznaj vise]";
+  }
+});
+//gore
+$(document).ready(function () {
+  jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 150) {
+      jQuery("#backtotop").addClass("visible");
+    } else {
+      jQuery("#backtotop").removeClass("visible");
+    }
+  });
+});
+//menjanje texta
+var recenica=document.getElementById("recenica");
+var text=["&ldquo;Pouzdani&bdquo;","&ldquo;Pristupačni&bdquo;","&ldquo;Uvek spremni za sradanju&bdquo;","&ldquo;Iskusni&bdquo;","&ldquo;Profesionalni&bdquo;"]
+var brojac=0;
+function Smenjivanje(){
+  recenica.innerHTML=text[brojac];
+  brojac++;
+if(brojac>4)brojac=0;
+
+}
+setInterval(Smenjivanje,2000)
