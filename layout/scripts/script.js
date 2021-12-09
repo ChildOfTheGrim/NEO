e16fba959c59c5cbe08f1313fc0dd4906435700c
ChildOfTheGrim <83035641+ChildOfTheGrim@users.noreply.github.com>
2021-12-09 17:22:26 +0100

    izmene

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -37,3 +37,34 @@ function Smenjivanje() {
   if (brojac > 4) brojac = 0;
 }
 setInterval(Smenjivanje, 2000);
+//jqveri modal
+$(function(){
+
+  var appendthis =  ("<div class='modal-overlay js-modal-close'></div>");
+  
+    $('a[data-modal-id]').click(function(e) {
+      e.preventDefault();
+      $("body").append(appendthis);
+      $(".modal-overlay").fadeTo(500, 0.7);
+      //$(".js-modalbox").fadeIn(500);
+      var modalBox = $(this).attr('data-modal-id');
+      $('#'+modalBox).fadeIn($(this).data());
+    });  
+    
+    
+  $(".js-modal-close, .modal-overlay").click(function() {
+    $(".modal-box, .modal-overlay").fadeOut(500, function() {
+      $(".modal-overlay").remove();
+    });
+  });
+   
+  $(window).resize(function() {
+    $(".modal-box").css({
+      top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
+      left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+    });
+  });
+   
+  $(window).resize();
+   
+  });
