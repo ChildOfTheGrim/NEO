7fb14a728f7635375cfbeefb47f012746b578c4e
ChildOfTheGrim <83035641+ChildOfTheGrim@users.noreply.github.com>
2021-12-10 17:24:36 +0100

    pus

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -1,15 +1,7 @@
+//skracivanje koda
 let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
-const hamburger=document.querySelector(".hamburger")
-const navMenu=document.querySelector(".nav_menu")
-hamburger.addEventListener("click",()=>{
-  hamburger.classList.toggle("active")
-  navMenu.classList.toggle("active")
-})
-document.querySelectorAll(".nav_link").forEach(n=>n.addEventListener("click",()=>{
-  hamburger.classList.remove("active")
-  navMenu.classList.remove("active")
-}))
+
 //saznaj vise
 let dugme = id("more_dugme");
 let dodatniText = id("text_vise");
@@ -23,6 +15,7 @@ dugme.addEventListener("click", function () {
     dugme.innerHTML = " [Saznaj vise]";
   }
 });
+
 //forma
 let padajuci = id("padajuca_lista");
 
@@ -66,11 +59,13 @@ let form = id("form"),
   email = id("email"),
   telefon = id("tel"),
   termin = id("termin"),
-  usluga = document.getElementsByName("opcija"), successIcon = classes("success-icon"),
+  usluga = document.getElementsByName("opcija"),
+  successIcon = classes("success-icon"),
   failureIcon = classes("failure-icon");
-  greska = classes("greska");
+greska = classes("greska");
 var brojGresaka;
-const regIme=/^[A-ZČĆŽŠĐ][a-zčćžšđ]{2,15}$/,regEmail= /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+const regIme = /^[A-ZČĆŽŠĐ][a-zčćžšđ]{2,15}$/,
+  regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 form.addEventListener("submit", (e) => {
   brojGresaka = 0;
   e.preventDefault();
@@ -78,32 +73,32 @@ form.addEventListener("submit", (e) => {
   engine(email, 1, "Email ne može biti prazan!");
   engine(telefon, 2, "Telefon ne može biti prazan!");
   engine(termin, 3, "Termin ne može biti prazan!");
-  if (brojGresaka == 0) {alert("Forma uspešno odrađena");  }
+  if (brojGresaka == 0) {
+    alert("Forma uspešno odrađena");
+  }
 });
 form.addEventListener("reset", (e) => {
   e.preventDefault();
- 
-  for(let i=0;i<4;i++){
+
+  for (let i = 0; i < 4; i++) {
     reset(i);
   }
 });
-let reset=(serial)=>{
-  failureIcon[serial].classList.add("prazno")
+let reset = (serial) => {
+  failureIcon[serial].classList.add("prazno");
   successIcon[serial].classList.add("prazno");
   greska[serial].innerHTML = "";
-}
+};
 let engine = (id, serial, poruka) => {
   if (id.value.trim() == "") {
     greska[serial].innerHTML = poruka;
-    failureIcon[serial].classList.remove("prazno")
+    failureIcon[serial].classList.remove("prazno");
     successIcon[serial].classList.add("prazno");
     brojGresaka++;
-
   } else {
     greska[serial].innerHTML = "";
-    failureIcon[serial].classList.add("prazno")
+    failureIcon[serial].classList.add("prazno");
     successIcon[serial].classList.remove("prazno");
-
   }
 };
 
@@ -123,6 +118,7 @@ function Smenjivanje() {
   if (brojac > 4) brojac = 0;
 }
 setInterval(Smenjivanje, 2000);
+
 //jqveri modal
 $(function () {
   var appendthis = "<div class='modal-overlay js-modal-close'></div>";
@@ -161,3 +157,80 @@ $(document).ready(function () {
     }
   });
 });
+// burger menu
+var burgerMenu = document.getElementById("burger-menu");
+var overlay = document.getElementById("menu");
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+//usluge generation
+
+let iNiz = [
+  "fas fa-balance-scale",
+  "fas fa-microchip",
+  "fas fa-hdd",
+  "fas fa-code",
+  "fas fa-laptop",
+  "fas fa-keyboard",
+];
+let h6Niz = [
+  "Programiranje biosa",
+  "Servis matičnih ploča",
+  "Spašavanje podataka",
+  "Instalacija OS",
+  "Redovan servis",
+  "Kompjuterska opreme",
+];
+let pNiz = [
+  "Usluga podrazumeva programiranje BIOS čipa i kao ioptimizaciju BIOS opcija.",
+  "Zamena Čipseta, Severnog i Južnog mosta, Grafičkog čipa, kao i svih ostalih vrsta BGA kola.",
+  "Odmah saznajte vrstu kvara. Besplatna dijagnoza i procena uspešnosti spašavanja podataka.",
+  "Instalacija sistema po izboru na laptopu ili desktop računaru.e",
+  "Servis, održavanje i čiščenje sistema, računara i mreža.",
+  "Oprema za održavanje i delovi za sve deskop, apple i ostalebrend računare.",
+];
+
+let sectionAppend = document.getElementById("introblocks");
+
+let newUl = document.createElement("ul");
+
+newUl.setAttribute("class", "nonspace");
+newUl.classList.add("group");
+newUl.classList.add("elements");
+newUl.classList.add("elements-three");
+
+for (var i = 0; i < 6; i++) {
+  let newLi = document.createElement("li");
+  newLi.setAttribute("class", "one_third");
+
+  let newArticle = document.createElement("article");
+  let newFooter = document.createElement("footer");
+  let aFooter = document.createElement("a");
+  let iFooter = document.createElement("i");
+
+  iFooter.setAttribute("class", `${iNiz[i]}`);
+
+  let articleHeader = document.createElement("h6");
+  articleHeader.setAttribute("class", "heading");
+  articleHeader.innerHTML = h6Niz[i];
+
+  let articleP = document.createElement("p");
+  articleP.innerHTML = pNiz[i];
+
+  aFooter.setAttribute("href", "#cenovnik");
+
+  aFooter.appendChild(iFooter);
+
+  newFooter.appendChild(aFooter);
+  newArticle.appendChild(newFooter);
+  newArticle.appendChild(articleHeader);
+  newArticle.appendChild(articleP);
+
+  newLi.appendChild(newArticle);
+
+  newUl.appendChild(newLi);
+}
+
+sectionAppend.appendChild(newUl);
