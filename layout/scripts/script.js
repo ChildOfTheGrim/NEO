899788e8159fbfac50ba051b6ae6ac700e1cebcd
ChildOfTheGrim <83035641+ChildOfTheGrim@users.noreply.github.com>
2021-12-11 01:48:27 +0100

    izmeneee

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -20,7 +20,7 @@ dugme.addEventListener("click", function () {
 let padajuci = id("padajuca_lista");
 
 var opcije = [
-  "",
+  "Izaberite",
   "Redovan Servis",
   "Programiranje Biosa",
   "Servis Matičnih Ploča",
@@ -28,24 +28,14 @@ var opcije = [
   "Instalacija OS",
   "Popravka uređaja",
 ];
-var vrednosti = [
-  "",
-  "servis",
-  "bios",
-  "maticna",
-  "podaci",
-  "instalacija",
-  "popravka",
-];
 
 let lista = document.createElement("select");
 lista.setAttribute("name", "problem");
 lista.setAttribute("id", "problem");
-//lista.setAttribute("required", "required");
 
 for (let i = 0; i < opcije.length; i++) {
   var novaOpcija = document.createElement("option");
-  novaOpcija.setAttribute("value", vrednosti[i]);
+  novaOpcija.setAttribute("value", `${i}`);
   novaOpcija.setAttribute("name", "opcija");
   novaOpcija.innerHTML = opcije[i];
   lista.appendChild(novaOpcija);
@@ -59,74 +49,70 @@ let form = id("form"),
   email = id("email"),
   telefon = id("tel"),
   termin = id("termin"),
-  vreme=id("time"),
-  usluga = document.getElementsByName("opcija"),
+  vreme = id("time"),
+  opcija = id("problem"),
   successIcon = classes("success-icon"),
   failureIcon = classes("failure-icon"),
-greska = classes("greska");
+  greska = classes("greska");
 var brojGresaka;
 
 const regIme = /^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}\s[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}$/,
   regEmail = /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
-regTermin = /^(0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2}$/,
-  regTel=/^06[0-689]\s[\d]{2}\s[\d]{2}\s[\d]{2,3}$/;
+  regTermin =
+    /^(0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2}$/,
+  regTel = /^06[0-689]\s[\d]{2}\s[\d]{2}\s[\d]{2,3}$/;
 
 form.addEventListener("submit", (e) => {
   brojGresaka = 0;
   e.preventDefault();
-  if(regIme.test(ime.value)){
+  if (regIme.test(ime.value)) {
     greska[0].innerHTML = "";
     failureIcon[0].classList.add("prazno");
     successIcon[0].classList.remove("prazno");
-  }
-  else{
+  } else {
     greska[0].innerHTML = "Ime i Prezime pogrešno uneti";
     failureIcon[0].classList.remove("prazno");
     successIcon[0].classList.add("prazno");
     brojGresaka++;
   }
-  if(regEmail.test(email.value)){
+  if (regEmail.test(email.value)) {
     greska[1].innerHTML = "";
     failureIcon[1].classList.add("prazno");
     successIcon[1].classList.remove("prazno");
-  }
-  else{
+  } else {
     greska[1].innerHTML = "Email pogresno unet";
     failureIcon[1].classList.remove("prazno");
     successIcon[1].classList.add("prazno");
     brojGresaka++;
   }
-  if(regTel.test(telefon.value)){
+  if (regTel.test(telefon.value)) {
     greska[2].innerHTML = "";
     failureIcon[2].classList.add("prazno");
     successIcon[2].classList.remove("prazno");
-  }
-  else{
+  } else {
     greska[2].innerHTML = "Broj telefona pogrešno unet";
     failureIcon[2].classList.remove("prazno");
     successIcon[2].classList.add("prazno");
     brojGresaka++;
   }
+  if (parseInt(opcija.value) == 0) {
+    greska[5].innerHTML = "Niste izabrali uslugu";
+    failureIcon[5].classList.remove("prazno");
+    successIcon[5].classList.add("prazno");
+    brojGresaka++;
+  } else {
+    greska[5].innerHTML = "";
+    failureIcon[5].classList.add("prazno");
+    successIcon[5].classList.remove("prazno");
+  }
 
-  // if(usluga.options[usluga.options.selectedIndex].value=="0"){
-
-  //   greska[4].innerHTML = "Ime i Prezime pogrešno uneti";
-  //   failureIcon[4].classList.remove("prazno");
-  //   successIcon[4].classList.add("prazno");
-  //   brojGresaka++;
-  // }
-  // else{
-  //   greska[4].innerHTML = "";
-  //   failureIcon[4].classList.add("prazno");
-  //   successIcon[4].classList.remove("prazno");
-  // }
   if (brojGresaka == 0) {
     alert("Forma uspešno odrađena");
   }
 });
 form.addEventListener("reset", (e) => {
   e.preventDefault();
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 6; i++) {
     reset(i);
   }
 });
@@ -134,21 +120,9 @@ let reset = (serial) => {
   failureIcon[serial].classList.add("prazno");
   successIcon[serial].classList.add("prazno");
   greska[serial].innerHTML = "";
-};
-let engine = (id, serial, poruka) => {
-  if (id.value.trim() == "") {
-    greska[serial].innerHTML = poruka;
-    failureIcon[serial].classList.remove("prazno");
-    successIcon[serial].classList.add("prazno");
-
-    brojGresaka++;
-  } else {
-    greska[serial].innerHTML = "";
-    failureIcon[serial].classList.add("prazno");
-    successIcon[serial].classList.remove("prazno");
-  }
 
 };
+
 
 //menjanje texta
 var recenica = id("recenica");
@@ -225,28 +199,28 @@ for (let i = 0; i < 3; i++) {
 
 //usluge generation
 let iNiz = [
-  "fas fa-balance-scale",
-  "fas fa-microchip",
-  "fas fa-hdd",
-  "fas fa-code",
-  "fas fa-laptop",
-  "fas fa-keyboard",
+  "fas fa-clock",
+  "fas fa-hand-holding-usd",
+  "fas fa-edit",
+  "fas fa-file-alt",
+  "fas fa-tachometer-alt",
+  "fas fa-shipping-fast",
 ];
 let h6Niz = [
-  "Programiranje biosa",
-  "Servis matičnih ploča",
-  "Spašavanje podataka",
-  "Instalacija OS",
-  "Redovan servis",
-  "Kompjuterska oprema",
+  "Radno vreme",
+  "Odkup i Prodaja",
+  "Dijagnostika",
+  "Garancija",
+  "Brzina",
+  "Popravka na terenu",
 ];
 let pNiz = [
-  "Usluga podrazumeva programiranje BIOS čipa kao i optimizaciju BIOS opcija.",
-  "Zamena Čipseta, Severnog i Južnog mosta, Grafičkog čipa, kao i svih ostalih vrsta BGA kola.",
-  "Odmah saznajte vrstu kvara. Besplatna dijagnoza i procena uspešnosti spašavanja podataka.",
-  "Instalacija sistema po izboru na laptopu ili desktop računaru.",
-  "Servis, održavanje i čišćenje sistema, računara i mreža.",
-  "Oprema za održavanje i delovi za sve deskop, apple i ostalebrend računare.",
+  "Ponedeljak - petak 9h-18h \nSubota i Nedelja neradni dani",
+  "Odkup i prodaja novih i polovnihi delova za laptop i desktop računare",
+  "Besplatna dijagnostika kvarova za računare koje servisiramo.",
+  "Garancija: 1-6 meseci na ugrađene komponente.",
+  "Zadržavanje računara u servisu zavisi od težine problema. Manji problemi rešavaju se obično istog ili sledećeg dana, a razne popravke za 1-3 dana. ",
+  "Ukoliko ste na teritoriji Beograda, dovoljno je da nas pozovete i mi dolazimo kod vas kako bi servisirali računare.",
 ];
 
 let sectionAppend = id("introblocks");
@@ -276,7 +250,7 @@ for (var i = 0; i < 6; i++) {
   let articleP = document.createElement("p");
   articleP.innerHTML = pNiz[i];
 
-  aFooter.setAttribute("href", "#cenovnik");
+  aFooter.setAttribute("href", "#usluge");
 
   aFooter.appendChild(iFooter);
 
